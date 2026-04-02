01c67d2063ea7c81c20f61980c0aae5528a95de7
travka677 <danikkim19@gmail.com>
2026-04-02 20:13:09 +0300

    add jacoco and sonar

--- src/test/java/com/innowise/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/innowise/orderservice/OrderServiceApplicationTests.java
@@ -1,9 +1,7 @@
 package com.innowise.orderservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class OrderServiceApplicationTests {
 
     @Test
